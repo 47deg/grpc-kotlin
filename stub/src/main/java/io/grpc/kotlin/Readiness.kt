cc34b649643569a2191ae5f15b543eb6090932be
danimontoya <daniel.montoya@47deg.com>
2020-08-24 16:39:09 +0200

    Replace conflated channel within Readiness with sliding queue of size 1

--- stub/src/main/java/io/grpc/kotlin/Readiness.kt
+++ stub/src/main/java/io/grpc/kotlin/Readiness.kt
@@ -16,20 +16,20 @@
 
 package io.grpc.kotlin
 
-import kotlinx.coroutines.channels.Channel
+import arrow.fx.coroutines.stream.concurrent.Queue
 
 /**
  * A simple helper allowing a notification of "ready" to be broadcast, and waited for.
  */
-internal class Readiness(
+internal class Readiness (
   private val isReallyReady: () -> Boolean
 ) {
   // A CONFLATED channel never suspends to send, and two notifications of readiness are equivalent
   // to one
-  private val channel = Channel<Unit>(Channel.CONFLATED)
+  private val channel = Queue.unsafeSliding<Unit>(1)
 
   fun onReady() {
-    if (!channel.offer(Unit)) {
+    if (!channel.tryOffer1(Unit)) {
       throw AssertionError(
         "Should be impossible; a CONFLATED channel should never return false on offer"
       )
@@ -38,64 +38,7 @@ internal class Readiness(
 
   suspend fun suspendUntilReady() {
     while (!isReallyReady()) {
-      channel.receive()
+      channel.dequeue1()
     }
   }
 }
-
-///*
-// * Copyright 2020 gRPC authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// * http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package io.grpc.kotlin
-//
-//import arrow.fx.coroutines.stream.concurrent.Queue
-//
-///**
-// * A simple helper allowing a notification of "ready" to be broadcast, and waited for.
-// */
-//internal class Readiness (
-//  private val isReallyReady: () -> Boolean
-//) {
-//  // A CONFLATED channel never suspends to send, and two notifications of readiness are equivalent
-//  // to one
-//  /**
-//   * are these equivalent? unsafeBounded(1)[Strategy.boundedFifo] and Channel<Unit>(Channel.CONFLATED)
-//   * Channel.CONFLATED: Channel that buffers at most one element and conflates all subsequent `send` and `offer` invocations,
-//   * so that the receiver always gets the most recently sent element.
-//   * Back-to-send sent elements are _conflated_ -- only the the most recently sent element is received,
-//   * while previously sent elements **are lost**.
-//   * Sender to this channel never suspends and [offer] always returns `true`.
-//   *
-//   * This channel is created by `Channel(Channel.CONFLATED)` factory function invocation.
-//   *
-//   * This implementation is fully lock-free.
-//   */
-//  private val channel = Queue.unsafeBounded<Unit>(1)
-//
-//  fun onReady() {
-//    if (!channel.tryOffer1(Unit)) {
-//      throw AssertionError(
-//        "Should be impossible; a CONFLATED channel should never return false on offer"
-//      )
-//    }
-//  }
-//
-//  suspend fun suspendUntilReady() {
-//    while (!isReallyReady()) {
-//      channel.dequeue1()
-//    }
-//  }
-//}
